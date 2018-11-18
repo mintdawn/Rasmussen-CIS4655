e29baef2c7a0b4731472be201b7a034fda4e0daa
mintdawn <38255303+mintdawn@users.noreply.github.com>
2018-11-17 22:44:56 -0600

    Update booklist.php

--- module2/project/booklist.php
+++ module2/project/booklist.php
@@ -14,7 +14,7 @@
       <?php include 'navbar3.html';?>
     <div class="container w-100">
       <div class="row">
-        <div class="col-sm-3 p-2">
+        <div class="col-sm-2 p-2">
           <h1>Title</h1>
           <hr>
           <ul>
@@ -28,30 +28,60 @@
             <li><a href="#" class="text-success">Example Link</a></li>
           </ul>
         </div>
-        <div class="col-sm-9 mx-auto p-2">
+        <div class="col-sm-10 mx-auto p-2">
           <h1>Title</h1>
           <hr>
-          <div class="card m-2 mx-auto rounded" style="width: 18rem;">
-            <img class="card-img-top" src="https://via.placeholder.com/300x100" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-success">Go somewhere</a>
-            </div>
-          </div>
-          <div class="card m-2 mx-auto rounded" style="width: 18rem;">
-            <img class="card-img-top" src="https://via.placeholder.com/300x100" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-success">Go somewhere</a>
-            </div>
+            <div class="container bg-secondary mx-auto p-4 rounded">
+              <div class="row">
+                <div class="col">
+                  <div class="card m-2 rounded">
+                    <img class="card-img-top img-fluid" src="https://via.placeholder.com/300x100" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-success">Go somewhere</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card m-2 rounded">
+                    <img class="card-img-top img-fluid" src="https://via.placeholder.com/300x100" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-success">Go somewhere</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <div class="card m-2 rounded">
+                    <img class="card-img-top img-fluid" src="https://via.placeholder.com/300x100" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-success">Go somewhere</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card m-2 rounded">
+                    <img class="card-img-top img-fluid" src="https://via.placeholder.com/300x100" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-success">Go somewhere</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
           </div>
         </div>
       </div>
     </div>
   <div class="container-fluid w-100 bg-light rounded text-secondary p-4">
-    <h2>A Bootstrap Carousel Displaying a Book</h2>
+    <h2>A Page with 2 Sections</h2>
     <hr>
     <p>This page was done for a school assignment. It was based on a wireframe I did in class. This page is supposed to contain a navbar with a logo at the left, some links and a non-functional search form but, I am using the page title instead of the search form. It is supposed to contain one smaller column with a title, hr and a list of links. To the right of this should be a larger column that contains a title, hr and several information cards. This page is supposed to look a like a modernized version of an html page with frames.</p>
   </div>
